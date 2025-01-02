39921f394511b57f7e3145e2c2dec4b7c83a1668
Luke <luke61996@gmail.com>
2025-01-02 21:11:42 +0000

    Added simple authenication.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"webserver/controllers"
 	"webserver/initializers"
+	middlewares "webserver/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,14 +19,14 @@ func main() {
 	r.StaticFile("/favicon.ico", "./favicon.ico")
 	r.Static("/static", "./static")
 	r.LoadHTMLGlob("templates/*.html")
-	r.GET("/", controllers.GetHomePageHandler)
-	r.POST("/", controllers.PostHomePageHandler)
+	r.GET("/", middlewares.CheckAuth, controllers.GetHomePageHandler)
+	r.POST("/", middlewares.CheckAuth, controllers.PostHomePageHandler)
 	r.POST("/log", controllers.InsertLog)
 	r.PUT("/fileclosed", controllers.UpdateTimeClosed)
 	r.GET("/login", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "login.html", gin.H{})
-
+		ctx.HTML(http.StatusOK, "login2.html", gin.H{})
 	})
+	r.POST("/login", controllers.Login)
 	err := r.RunTLS(":443", initializers.EnvFile["CERT"], initializers.EnvFile["CERT_KEY"])
 	if err != nil {
 		log.Fatal(err)
---
+++ middleware/checkAuth.go
@@ -0,0 +1,66 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+	"webserver/initializers"
+	"webserver/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+)
+
+func CheckAuth(c *gin.Context) {
+
+	authHeader, err := c.Cookie("Authorization")
+
+	if authHeader == "" {
+
+		c.HTML(http.StatusOK, "login2.html", gin.H{
+			"error_message": "Need to be authorized to access that page",
+		})
+		c.Abort()
+		return
+	}
+	fmt.Println(authHeader)
+	tokenString := authHeader
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("SECRET")), nil
+	})
+	if err != nil || !token.Valid {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or expired token"})
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		c.Abort()
+		return
+	}
+
+	if float64(time.Now().Unix()) > claims["exp"].(float64) {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "token expired"})
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	var user models.User
+	initializers.DB.Where("ID=?", claims["id"]).Find(&user)
+
+	if user.ID == 0 {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	c.Set("currentUser", user)
+
+	c.Next()
+
+}
