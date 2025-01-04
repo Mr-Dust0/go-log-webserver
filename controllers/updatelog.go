117160f0a0fc1f8a20bdeaa88a8e7af2ce333a96
Luke <luke61996@gmail.com>
2025-01-04 14:01:15 +0000

    Added comments to updatelog.go

--- controllers/updatelog.go
+++ controllers/updatelog.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 	"webserver/initializers"
@@ -12,9 +11,9 @@ import (
 
 func UpdateTimeClosed(ctx *gin.Context) {
 	var request struct {
-		HostName   string    `json:"hostname"`
-		TimeClosed time.Time `json:"TimeStampClosed"`
+		HostName string `json:"hostname"`
 	}
+	// Get json data from qr-code reqder and store it in request
 	if err := ctx.ShouldBindJSON(&request); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
 		return
@@ -22,7 +21,7 @@ func UpdateTimeClosed(ctx *gin.Context) {
 
 	// Better to do the timestamp on the server side beacuse the time on the raspberry pi can be incorrect since it does not have an battery powered clock so time will be off until ntp fixes it.
 	parsedDate := time.Now().Format("2006-01-02")
-	fmt.Println(parsedDate)
+	// Update TImeStampClosed for matching hostname and where TimeStampClosed is still null
 	result := initializers.DB.Model(&models.LogEntry{}).
 		Where("host_name = ? AND DATE(time_stamp) = ? AND time_stamp_closed = '0001-01-01 00:00:00+00:00'", request.HostName, parsedDate).
 		Update("time_stamp_closed", time.Now())
@@ -30,5 +29,6 @@ func UpdateTimeClosed(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update log"})
 		return
 	}
+	// Send sucess message back to qr-code reader
 	ctx.JSON(http.StatusOK, gin.H{"status": "File closed successfully"})
 }
