b124ae34f031b40029775c7518482885a5461323
Luke <luke61996@gmail.com>
2025-01-04 14:13:05 +0000

    Added comments to loadEnvs.go

--- initializers/loadEnvs.go
+++ initializers/loadEnvs.go
@@ -6,10 +6,12 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// Enable EnvFile to be accessed from anywhere
 var EnvFile map[string]string
 
 func LoadEnvs() {
 	var err error
+	// load .env file that config in
 	EnvFile, err = godotenv.Read()
 	if err != nil {
 		//log.Fatal("Error loading .env file")
