54051f801f111294bc32229ee76305fed501ed2e
Luke <luke61996@gmail.com>
2025-01-04 13:45:06 +0000

    Added comments to authController.go

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 	"time"
@@ -17,6 +16,7 @@ func Login(ctx *gin.Context) {
 
 	var authInput models.AuthInput
 
+	// Take Json from POST request from login.html and put the data into authInput
 	if err := ctx.ShouldBindJSON(&authInput); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -25,28 +25,32 @@ func Login(ctx *gin.Context) {
 	var userFound models.User
 	initializers.DB.Where("username=?", authInput.Username).Find(&userFound)
 
+	// Check if the Username matches with an user
 	if userFound.ID == 0 {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
 		return
 	}
 
+	// Hash the Password given in the POST request and compare it to the hash in the database to see if they are the same
 	if err := bcrypt.CompareHashAndPassword([]byte(userFound.Password), []byte(authInput.Password)); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid password"})
 		return
 	}
 
+	// Create an jwt token with the userId in the claims and expires in 24 hours so they user doesnt have to keep logining back in.
 	generateToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"id":  userFound.ID,
 		"exp": time.Now().Add(time.Hour * 24).Unix(),
 	})
 
+	// Sign the token so end users cant change the claims and become an differnt user or some kind of attack
 	token, err := generateToken.SignedString([]byte(os.Getenv("SECRET")))
 
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "failed to generate token"})
 	}
 
-	fmt.Println(userFound.Email)
+	// Set Authorization cookie that can last for an day and allow to be sent over http because securecookie is set to false.
 	ctx.SetCookie("Authorization", token, 24*3600, "", "", false, true)
 }
 func ChangePassword(ctx *gin.Context) {
@@ -54,22 +58,28 @@ func ChangePassword(ctx *gin.Context) {
 	userName := ctx.PostForm("username")
 	oldPassword := ctx.PostForm("oldpassword")
 	newPassword := ctx.PostForm("newpassword")
+	// Check to see if the password has actaully changed
 	if newPassword == oldPassword {
 		ctx.HTML(http.StatusConflict, "resetpassword.html", gin.H{"error_message": "Old password cannot equal new password"})
 		return
 	}
 	var user models.User
 	initializers.DB.Where("userName = ?", userName).Find(&user)
+	// Check if the user exists
 	if user.ID == 0 {
 		// Dont want to say the username is wrong because that can give attackers an idea of what usernames to brute force
 		ctx.HTML(http.StatusConflict, "resetpassword.html", gin.H{"error_message": "Invalid user details"})
 		return
 	}
+	// Hash the old password and see if matches the one stored in the database
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(oldPassword)); err != nil {
 		ctx.HTML(http.StatusConflict, "resetpassword.html", gin.H{"error_message": "Invalid user details"})
 		return
 	}
-	initializers.DB.Where("ID = ?", user.ID).Update("Password", newPassword)
+	// Hash new password before storing it in database
+	newPasswordHash, _ := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
+	// Update password to new password that is hashed
+	initializers.DB.Where("ID = ?", user.ID).Update("Password", newPasswordHash)
 	ctx.HTML(http.StatusOK, "index.html", gin.H{"email": user.Email})
 }
 func GetResetPage(ctx *gin.Context) {
