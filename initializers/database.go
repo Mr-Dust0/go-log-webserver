0c886738e6ae75c6134ffd5a49c2a6e2193169b5
Luke <luke61996@gmail.com>
2025-01-04 11:21:47 +0000

    Added sample log data and hashed password for user before inserting

--- initializers/database.go
+++ initializers/database.go
@@ -5,8 +5,10 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 	"webserver/models"
 
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -36,13 +38,51 @@ func InitDatabase() {
 	}
 }
 func InsertTestData() {
+	passwordHash, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
 	sampleUser := models.User{
 		Email:    "test@test.com",
 		Username: "test",
-		Password: "password"}
+		Password: string(passwordHash)}
 	if err := DB.Create(&sampleUser).Error; err != nil {
-		log.Fatal("Could not create the sample user")
+		fmt.Println("Already created sample user")
 		return
 
+	}
+	logEntries := []models.LogEntry{
+		{
+			TimeStamp:       time.Date(2025, 1, 2, 15, 5, 47, 42288622, time.UTC),
+			TimeStampClosed: time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC), // Invalid date
+			HostName:        "debian",
+			FileName:        "/mnt/nfs/testdoc.pdf",
+		},
+		{
+			TimeStamp:       time.Date(2025, 1, 3, 9, 50, 29, 657848343, time.UTC),
+			TimeStampClosed: time.Date(2025, 1, 3, 9, 51, 47, 715981132, time.UTC),
+			HostName:        "raspberrypi",
+			FileName:        "/home/pi/009085-A Drawing v1.pdf",
+		},
+		{
+			TimeStamp:       time.Date(2025, 1, 3, 9, 57, 23, 461050454, time.UTC),
+			TimeStampClosed: time.Date(2025, 1, 3, 10, 2, 54, 111097126, time.UTC),
+			HostName:        "raspberrypi",
+			FileName:        "/home/pi/testdoc.pdf",
+		},
+		{
+			TimeStamp:       time.Date(2025, 1, 3, 10, 2, 57, 659195934, time.UTC),
+			TimeStampClosed: time.Date(2025, 1, 3, 10, 23, 22, 617727232, time.UTC),
+			HostName:        "raspberrypi",
+			FileName:        "/home/pi/009085-A Drawing v1.pdf",
+		},
+		{
+			TimeStamp:       time.Date(2025, 1, 3, 10, 23, 33, 658710661, time.UTC),
+			TimeStampClosed: time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC), // Invalid date
+			HostName:        "raspberrypi",
+			FileName:        "/home/pi/testdoc.pdf",
+		},
+	}
+
+	// Insert the records into the database
+	if err := DB.Create(&logEntries).Error; err != nil {
+		log.Fatal("Error inserting records for log enteries")
 	}
 }
