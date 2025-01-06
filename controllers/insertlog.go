65962a5172781d263fabfaa1654d5be1c031a36a
Luke <luke61996@gmail.com>
2025-01-06 13:51:40 +0000

    Added check to see if the hostname only has one file open

--- controllers/insertlog.go
+++ controllers/insertlog.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 	"webserver/initializers"
@@ -16,8 +17,13 @@ func InsertLog(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
 		return
 	}
-	// Generate Timestamp the file was open here because the pi time can be off
 	logEntry.TimeStamp = time.Now()
+	// Check to see if the hostname already has an file open and if so update the file to be closed this cloud happen if the pi was shut down without sending an closed request for the file it had open or the pi not having internet when the file was closed so need to check this.
+	err := initializers.DB.Where("time_stamp_closed = ? AND host_name = ?", "0001-01-01 00:00:00+00:00", logEntry.HostName).Update("time_stamp_closed", logEntry.TimeStamp)
+	if err != nil {
+		fmt.Println("Couldnt check whever that hostname already has an file open")
+	}
+	// Generate Timestamp the file was open here because the pi time can be off
 	// Create an new entry in the database for the log
 	if err := initializers.DB.Create(&logEntry).Error; err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to insert log"})
